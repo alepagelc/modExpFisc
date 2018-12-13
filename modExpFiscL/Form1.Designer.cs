c254bbe0feef2c6737938cbf34056f407d9ee38b
Arnaud Lepage <alepage@elcia.com>
2018-12-13 17:33:13 +0100

    départ

---
+++ modExpFiscL/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace modExpFiscL
+{
+    partial class FrmExport
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxBase = new System.Windows.Forms.ComboBox();
+            this.textBoxServeur = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExtraire = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxBase);
+            this.groupBox1.Controls.Add(this.textBoxServeur);
+            this.groupBox1.Controls.Add(this.textBoxPort);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 109);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sélectionner le serveur et la base de données pour laquelle réaliser l\'extraction" +
+    " :";
+            // 
+            // comboBoxBase
+            // 
+            this.comboBoxBase.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBase.FormattingEnabled = true;
+            this.comboBoxBase.Location = new System.Drawing.Point(9, 76);
+            this.comboBoxBase.Name = "comboBoxBase";
+            this.comboBoxBase.Size = new System.Drawing.Size(345, 21);
+            this.comboBoxBase.TabIndex = 4;
+            // 
+            // textBoxServeur
+            // 
+            this.textBoxServeur.Location = new System.Drawing.Point(247, 41);
+            this.textBoxServeur.Name = "textBoxServeur";
+            this.textBoxServeur.Size = new System.Drawing.Size(107, 20);
+            this.textBoxServeur.TabIndex = 3;
+            this.textBoxServeur.Text = "127.0.0.1";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(44, 41);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(40, 20);
+            this.textBoxPort.TabIndex = 2;
+            this.textBoxPort.Text = "5432";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adresse / Nom du serveur :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port :";
+            // 
+            // btnExtraire
+            // 
+            this.btnExtraire.Location = new System.Drawing.Point(148, 138);
+            this.btnExtraire.Name = "btnExtraire";
+            this.btnExtraire.Size = new System.Drawing.Size(75, 23);
+            this.btnExtraire.TabIndex = 1;
+            this.btnExtraire.Text = "Extraire";
+            this.btnExtraire.UseVisualStyleBackColor = true;
+            this.btnExtraire.Click += new System.EventHandler(this.btnExtraire_Click);
+            // 
+            // FrmExport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 179);
+            this.Controls.Add(this.btnExtraire);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmExport";
+            this.Text = "Exportation des données ficscales";
+            this.Load += new System.EventHandler(this.FrmExport_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxBase;
+        private System.Windows.Forms.TextBox textBoxServeur;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExtraire;
+    }
+}
+
---
+++ modExpFiscL/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using System.IO;
+
+namespace modExpFiscL
+{
+    public partial class FrmExport : Form
+    {
+        // Initialisation des variables
+        String portServeur = null;
+        String adrServeur = null;
+        String requetes = null;
+        String paramConnexion = null;
+        String loginPG = "postgres";
+        String passPG = "pgpass";
+        Boolean mesTests = true;
+
+        OdbcConnection chaineConnexionListeBases = new OdbcConnection();
+        OdbcCommand mesRequetes = new OdbcCommand();
+        OdbcDataReader retourRequetes;
+
+        public FrmExport()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmExport_Load(object sender, EventArgs e)
+        {
+            // Remplissage des textboxs
+            if (textBoxPort.Text == null)
+            {
+                textBoxPort.Text = "5432";
+                portServeur = "5432";
+            }
+            else
+            {
+                portServeur = textBoxPort.Text;
+            }
+
+            if (textBoxServeur.Text == null)
+            {
+                textBoxServeur.Text = "127.0.0.1";
+                adrServeur = "127.0.0.1";
+            }
+            else
+            {
+                adrServeur = textBoxServeur.Text;
+            }
+
+            // Initialisations des paramètres pour le passage de la requête
+            requetes = "SELECT datname FROM pg_database WHERE datistemplate=FALSE and datname not like 'postgres'";
+            paramConnexion = "Driver={PostgreSQL Unicode};Server=" + adrServeur + ";Port=" + portServeur + ";Database=postgres;Uid=" + loginPG + ";Pwd=" + passPG + ";";
+
+            using (chaineConnexionListeBases = new OdbcConnection(paramConnexion))
+            {
+                try
+                {
+                    // Passage de la commande
+                    mesRequetes = new OdbcCommand(requetes, chaineConnexionListeBases);
+                    // Ouverture
+                    chaineConnexionListeBases.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur à la connexion au serveur pour la récupération de la liste des bases de données.");
+                    mesTests = false;
+                }
+                finally
+                {
+                    // Exécution si pas d'exception
+                    if (mesTests)
+                    {
+                        // Exeécution de la requête
+                        retourRequetes = mesRequetes.ExecuteReader();
+
+                        // Remplissage du comboBox
+                        while (retourRequetes.Read())
+                        {
+                            comboBoxBase.Items.Add(retourRequetes["datname"]);
+                        }
+
+                        comboBoxBase.Text = comboBoxBase.Items[0].ToString();
+                    }
+                }
+            }
+        }
+
+        private void textBoxServeur_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+            if ((e.KeyCode == Keys.Return) && (textBoxPort.Text != null))
+            {
+                // Initialsation des variables des valeurs en textBoxs
+                portServeur = textBoxPort.Text;
+                adrServeur = textBoxServeur.Text;
+
+                // Initialisations des paramètres pour le passage de la requête
+                requetes = "SELECT datname FROM pg_database WHERE datistemplate=FALSE and datname not like 'postgres'";
+                paramConnexion = "Driver={PostgreSQL Unicode};Server=" + adrServeur + ";Port=" + portServeur + ";Database=postgres;Uid=" + loginPG + ";Pwd=" + passPG + ";";
+
+                // Si le port et l'adresse sont bien renseignés
+                // Mise à jour du comboBox
+                // Connexion à la base
+                if ((portServeur != null) && (adrServeur != null))
+                {
+                    using (chaineConnexionListeBases = new OdbcConnection(paramConnexion))
+                    {
+                        try
+                        {
+                            // Passage de la commande
+                            mesRequetes = new OdbcCommand(requetes, chaineConnexionListeBases);
+                            // Ouverture
+                            chaineConnexionListeBases.Open();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Erreur à la connexion à la base suite à la modification de l'adresse ou du nom du serveur, vérifiez le paramètre.");
+                        }
+                        finally
+                        {
+                            // Exécution si pas d'exception
+                            if (mesTests)
+                            {
+                                // Exeécution de la requête
+                                retourRequetes = mesRequetes.ExecuteReader();
+
+                                // On vide la comboBox
+                                comboBoxBase.Items.Clear();
+
+                                // Remplissage du comboBox
+                                while (retourRequetes.Read())
+                                {
+                                    comboBoxBase.Items.Add(retourRequetes["datname"]);
+                                }
+
+                                // Fermeture de la connexion
+                                retourRequetes.Close();
+
+                                // On récupère la première valeur
+                                comboBoxBase.Text = comboBoxBase.Items[0].ToString();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void textBoxPort_KeyUp(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.Return) && (textBoxServeur.Text != null))
+            {
+                // Initialisation des variables
+                mesTests = true;
+
+                // Récupération des valeurs en textBoxs
+                portServeur = textBoxPort.Text;
+                adrServeur = textBoxServeur.Text;
+
+                // Initialisations des paramètres pour le passage de la requête
+                requetes = "SELECT datname FROM pg_database WHERE datistemplate=FALSE and datname not like 'postgres'";
+                paramConnexion = "Driver={PostgreSQL UNICODE};Server=" + adrServeur + ";Port=" + portServeur + ";Database=postgres;Uid=" + loginPG + ";Pwd=" + passPG + ";";
+
+                // Si le port et l'adresse sont bien renseignés
+                // Mise à jour du comboBox
+                // Connexion à la base
+                if ((portServeur != null) && (adrServeur != null))
+                {
+                    using (chaineConnexionListeBases = new OdbcConnection(paramConnexion))
+                    {
+                        try
+                        {
+                            // Passage de la commande
+                            mesRequetes = new OdbcCommand(requetes, chaineConnexionListeBases);
+                            // Ouverture
+                            chaineConnexionListeBases.Open();
+                        }
+                        catch (Exception ex)
+                        {
+                            mesTests = false;
+                            MessageBox.Show("Erreur à la connexion à la base suite à la modification du port, vérifiez le paramètre.");
+                        }
+                        finally
+                        {
+                            // Exécution si pas d'exception
+                            if (mesTests)
+                            {
+                                // Exeécution de la requête
+                                retourRequetes = mesRequetes.ExecuteReader();
+
+                                // On vide la comboBox
+                                comboBoxBase.Items.Clear();
+
+                                // Remplissage du comboBox
+                                while (retourRequetes.Read())
+                                {
+                                    comboBoxBase.Items.Add(retourRequetes["datname"]);
+                                }
+
+                                // Fermeture de la connexion
+                                retourRequetes.Close();
+
+                                // On récupère la première valeur
+                                comboBoxBase.Text = comboBoxBase.Items[0].ToString();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnExtraire_Click(object sender, EventArgs e)
+        {
+            // Déclaration des variables
+            String requete1 = null;
+            String requeteTVA1 = null;
+            String requeteTVA2 = null;
+            String requeteTVA3 = null;
+            String requeteTVA4 = null;
+            String requeteART = null;
+            StreamWriter sw = new StreamWriter(Application.StartupPath + "\\extraction_prodevis.csv");
+            OdbcCommand maRequete1 = new OdbcCommand();
+            OdbcCommand maRequeteTVA1 = new OdbcCommand();
+            OdbcCommand maRequeteTVA2 = new OdbcCommand();
+            OdbcCommand maRequeteTVA3 = new OdbcCommand();
+            OdbcCommand maRequeteTVA4 = new OdbcCommand();
+            OdbcCommand maRequeteART = new OdbcCommand();
+            OdbcDataReader retourRequete1;
+            OdbcDataReader retourRequeteTVA1;
+            OdbcDataReader retourRequeteTVA2;
+            OdbcDataReader retourRequeteTVA3;
+            OdbcDataReader retourRequeteTVA4;
+            OdbcDataReader retourRequeteART;
+            int i = 0;
+
+            paramConnexion = "Driver={PostgreSQL UNICODE};Server=" + textBoxServeur.Text + ";Port=" + textBoxPort.Text + ";Database=" + comboBoxBase.Text + ";Uid=" + loginPG + ";Pwd=" + passPG + ";";
+
+            requete1 = "SELECT f.id_facture AS ID, f.numdoc AS NUMFACT, dtdoc AS DATFAC, CONCAT(lv.liste, ' ', cc.nom, ' ', cc.prenom) AS NOM, f.totalttc AS TOTTTC, f.taxe_taux1 AS TXTAXE1, taxe_taux2 AS TXTAXE2, taxe_taux3 AS TXTAXE3, taxe_taux4 AS TXTAXE4 FROM facture f, client_coordonnees cc, listvoca lv WHERE cc.titre_id = lv.id AND f.client_coordonnees_id = cc.id_client_coordonnees";
+            
+            if ((portServeur != null) && (adrServeur != null))
+            {
+                using (chaineConnexionListeBases = new OdbcConnection(paramConnexion))
+                {
+                    try
+                    {
+                        // Passage de la commande
+                        maRequete1 = new OdbcCommand(requete1, chaineConnexionListeBases);
+                        // Ouverture
+                        chaineConnexionListeBases.Open();
+                    }
+                    catch (Exception ex)
+                    {
+                        mesTests = false;
+                        MessageBox.Show("Erreur à la connexion à la base de données sélectionnée.");
+                        Application.Exit();
+                    }
+                    finally
+                    {
+                        // Exécution si pas d'exception
+                        if (mesTests)
+                        {
+                            // Exeécution de la requête
+                            retourRequete1 = maRequete1.ExecuteReader();
+                            try
+                            {
+                                // Remplissage du comboBox
+                                while (retourRequete1.Read())
+                                {
+                                    sw.Write(retourRequetes["NUMFACT"]);
+                                    sw.Write(";");
+                                    sw.Write(retourRequetes["DATFAC"]);
+                                    sw.Write(";");
+                                    sw.Write(retourRequetes["NOM"]);
+                                    sw.Write(";");
+                                    //sw.Write(retourRequetes["TOTTTC"]);
+
+                                    requeteTVA1 = "SELECT taxe_num, SUM(ROUND((prixventerpp*qte)::numeric,2))+SUM(ROUND((prixventepre2rpp*qte)::numeric,2)) AS PROD, SUM(ROUND((prixventepre1rpp*qte)::numeric,2)) AS PREST, SUM(ROUND((prix_ecotaxe*qte)::numeric,2)) AS ECO FROM ligne_affaire_facture WHERE produit_type = 'P' AND FACTURE_id=" + retourRequetes["ID"] + " AND taxe_num=1 GROUP BY taxe_num"
+                                    requeteTVA2 = "SELECT taxe_num, SUM(ROUND((prixventerpp*qte)::numeric,2))+SUM(ROUND((prixventepre2rpp*qte)::numeric,2)) AS PROD, SUM(ROUND((prixventepre1rpp*qte)::numeric,2)) AS PREST, SUM(ROUND((prix_ecotaxe*qte)::numeric,2)) AS ECO FROM ligne_affaire_facture WHERE produit_type = 'P' AND FACTURE_id=" + retourRequetes["ID"] + " AND taxe_num=2 GROUP BY taxe_num"
+                                    requeteTVA3 = "SELECT taxe_num, SUM(ROUND((prixventerpp*qte)::numeric,2))+SUM(ROUND((prixventepre2rpp*qte)::numeric,2)) AS PROD, SUM(ROUND((prixventepre1rpp*qte)::numeric,2)) AS PREST, SUM(ROUND((prix_ecotaxe*qte)::numeric,2)) AS ECO FROM ligne_affaire_facture WHERE produit_type = 'P' AND FACTURE_id=" + retourRequetes["ID"] + " AND taxe_num=3 GROUP BY taxe_num"
+                                    requeteTVA4 = "SELECT taxe_num, SUM(ROUND((prixventerpp*qte)::numeric,2))+SUM(ROUND((prixventepre2rpp*qte)::numeric,2)) AS PROD, SUM(ROUND((prixventepre1rpp*qte)::numeric,2)) AS PREST, SUM(ROUND((prix_ecotaxe*qte)::numeric,2)) AS ECO FROM ligne_affaire_facture WHERE produit_type = 'P' AND FACTURE_id=" + retourRequetes["ID"] + " AND taxe_num=4 GROUP BY taxe_num"
+                                    requeteART = "SELECT id_ligne_affaire_fact AS ID, designation AS DESIG FROM ligne_affaire_facture, CONCAT (options_produit, ' - ', options_prest) AS OPTIONS WHERE produit_type = 'P' AND FACTURE_id=" + retourRequetes["ID"];
+
+
+                                    try
+                                    {
+                                        // Passage de la commande
+                                        maRequeteTVA1 = new OdbcCommand(requete1, chaineConnexionListeBases);
+                                        // Ouverture
+                                        chaineConnexionListeBases.Open();
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        mesTests = false;
+                                        MessageBox.Show("Erreur à la connexion à la base de données sélectionnée pour lister les produits.");
+                                        Application.Exit();
+                                    }
+                                    finally
+                                    {
+                                        retourRequeteART = maRequeteTVA1.ExecuteReader();
+
+                                        i = 0;
+
+                                        while (retourRequeteART.Read())
+                                        {
+                                            if (i == 0)
+                                            {
+                                                sw.Write("Article " + i + " : " + retourRequeteART["DESIG"]);
+                                            }
+                                            else
+                                            {
+                                                sw.Write(" - Article " + i + " : " + retourRequeteART["DESIG"]);
+                                            }
+                                            i++;
+                                        }
+
+                                        i = 0;
+
+                                        while (retourRequeteART.Read())
+                                        {
+                                            if (i == 0)
+                                            {
+                                                sw.Write("Options article " + i + " : " + retourRequeteART["DESIGNATION"]);
+                                            }
+                                            else
+                                            {
+                                                sw.Write(" - Options article " + i + " : " + retourRequeteART["OPTIONS"]);
+                                            }
+                                            i++;
+                                        }
+
+
+                                        retourRequeteTVA1 = maRequeteTVA1.ExecuteReader();
+                                    }
+                                }
+                            }
+                            catch(Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+
+                            // Fermeture de la connexion
+                            retourRequete1.Close();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
